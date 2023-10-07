a7d90615a5be12a6018343c8d7bd96edb004b43b
GabrielVdoom <silvagueiros@hotmail.com>
2023-10-06 23:02:12 -0300

    first commit

---
+++ PROJECT IN ENGLISH/main.c
@@ -0,0 +1,369 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <strings.h>
+
+
+char user[100][10], password[100][20], user1[10], password1[20], productChoice[100][20], Adress[4][30], number[100];
+int attempts=0, totalusers=0, paymentChoice, unidadeCoffee=0, unidadeCheeseBread=0, unidadePizza=0, unidadeFries=0, unidadeHamburguer=0, unidadeSoda=0, unidadeJuice=0;
+float precoTotal=0, valordotroco, vtroco;
+
+
+void menu()
+{
+    printf("\n\t\t\t******WELCOME******\n\n\t\t\t-TYPE 1 TO REGISTER USER.\n\t\t\t-TYPE 2 TO LOGIN.\n\t\t\t-TYPE EXIT TO END PROGRAM.\n\n");
+    fflush(stdin);
+    scanf("%[^\n]", number);
+    int sexo=1;
+    while(sexo==1)
+    {if(strcmp(number,"1")==0)
+        {cadastrar();
+        return;
+        }
+
+    else if(strcmp(number,"2")==0)
+        {login();
+        return;
+        }
+
+    else if(stricmp(number,"exit")==0)
+        {printf("\nPROGRAM ENDED\n");
+        break;
+        }
+
+    else
+        {printf("\nINVALID COMMAND, TYPE AGAIN\n");
+         fflush(stdin);
+         scanf("%[^\n]", number);
+        }
+
+    }
+
+}
+
+void cadastrar()
+{
+    printf("\nEnter a username to create your login:\n\n");
+    scanf(" %[^\n]", user[totalusers]);
+
+    while (strchr(user[totalusers], ' ') != NULL)
+    {
+        printf("\nUsername can't contain spacebar, type in again:\n\n");
+        scanf(" %[^\n]", user[totalusers]);
+    }
+
+     for(int i=0; i<totalusers; i++)
+               while(strcmp(user[totalusers], user[i])==0)
+                {
+                printf("\nUsername already registred, type a new username or type 3 to go back to menu:\n\n");
+                scanf("%s", user[totalusers]);
+                if(strcmp(user[totalusers], "3")==0)
+                {   system("cls");
+                    menu();
+                }
+                }
+    printf("\nEnter a password:\n\n");
+    scanf("%s", password[totalusers]);
+    totalusers++;
+
+    printf("\nWell Done! User and password successfully registered\n");
+    printf("\nPress any key to return to menu\n\n");
+    getch();
+    system("cls");
+    menu();
+}
+
+void login()
+{
+    printf("\nEnter your username:\n\n");
+    scanf("%s", user1);
+
+    int achou=0;
+    int indexuser;
+    while (achou==0) {
+        for (int i = 0; i < totalusers; i++) {
+            if (strcmp(user1,user[i])==0) {
+                achou = 1;
+                indexuser = i;
+                break;
+            }
+        }
+
+        if (achou==0) {
+            printf("\nUser not registered, enter a valid user or press 3 to go back to menu:\n\n");
+            scanf("%s", user1);
+            if(strcmp(user1, "3")==0) {
+                system("cls");
+                menu();
+                return;
+            }
+        }
+    }
+
+    printf("\nEnter your password:\n\n");
+    scanf("%s", password1);
+
+    while(strcmp(password[indexuser], password1)!=0)
+
+        {   attempts++;
+            if(attempts==1)
+            {
+            printf("\nACESSO DENIED, TRY AGAIN, 3 attempts left:\n\n");
+            scanf("%s", password1);
+            }
+            if(attempts==2)
+            {
+            printf("\nACESSO DENIED, TRY AGAIN, 2 attempts left:\n\n");
+            scanf("%s", password1);
+            }
+            if(attempts==3)
+            {
+            printf("\nACESSO DENIED, TRY AGAIN, 1 attempt left:\n\n");
+            scanf("%s", password1);
+            }
+            if(attempts==4)
+                printf("\nACESS BLOCKED\n\n");
+
+    }
+
+    printf("\nACESSO GRANTED!\n\n");
+    printf("Press any key to acess the Virtual Cafeteria...\n\n");
+    getch();
+    menulanchonete();
+}
+
+
+void menulanchonete()
+{
+    system("cls");
+    printf("\n\t******Welcome to Virtual Cafeteria******\n\n");
+
+    printf(" 1.Coffee----------$5,00\n");
+    printf(" 2.Cheese Bread----$4,00\n");
+    printf(" 3.Pizza-----------$5,50\n");
+    printf(" 4.Fries-----------$6,00\n");
+    printf(" 5.Hamburguer------$5,00\n");
+    printf(" 6.Soda------------$3,50\n");
+    printf(" 7.Juice-----------$3,00\n\n");
+
+    printf("Type in your order, one by one, then press 0 to end your order and proceed to checkout:\n\n");
+
+    for(int i=1; i<=100000;i++)
+    {fflush(stdin);
+        scanf("%[^\n]", productChoice);
+        if(strcmp(productChoice,"0")==0)
+         {carrinho();
+         return;
+         }
+         if(stricmp(productChoice,"coffee")==0 || strcmp(productChoice,"1")==0)
+           {precoTotal+=5;
+           unidadeCoffee++;
+           }
+         else if(stricmp(productChoice,"cheese bread")==0 || strcmp(productChoice,"2")==0)
+         {precoTotal+=4;
+         unidadeCheeseBread++;
+         }
+        else if(stricmp(productChoice,"pizza")==0 || strcmp(productChoice,"3")==0)
+           {precoTotal+=5.5;
+            unidadePizza++;
+           }
+        else if(stricmp(productChoice,"fries")==0 || strcmp(productChoice,"4")==0)
+         {precoTotal+=6;
+         unidadeFries++;
+         }
+        else if(stricmp(productChoice,"hamburguer")==0 || strcmp(productChoice,"5")==0)
+           {precoTotal+=5;
+            unidadeHamburguer++;
+           }
+        else if(stricmp(productChoice,"soda")==0 || strcmp(productChoice,"6")==0)
+         {precoTotal+=3.5;
+          unidadeSoda++;
+         }
+        else if(stricmp(productChoice,"juice")==0 || strcmp(productChoice,"7")==0)
+           {precoTotal+=3;
+           unidadeJuice++;
+           }
+        else
+        {
+        printf("\nItem not registered, try again\n\n");
+        }
+
+      }
+
+    }
+
+void carrinho()
+{
+     if(unidadeCoffee==0 && unidadeCheeseBread==0 && unidadePizza==0 && unidadeFries==0 && unidadeHamburguer==0 && unidadeSoda==0 && unidadeJuice==0)
+        {
+         system("cls");
+         printf("\nChoose at least 1 item to proceed to checkout:\n\n");
+         printf("Press any key to go back to the cafeteria menu...");
+         getch();
+         menulanchonete();
+
+        }
+    system("cls");
+    printf("\n***CART***\n");
+
+    if(unidadeCoffee==1)
+        printf("--%d Cup of Coffee\n", unidadeCoffee);
+        else if(unidadeCoffee>1)
+        printf("--%d Cups of Coffee\n", unidadeCoffee);
+
+     if(unidadeCheeseBread==1)
+        printf("--%d Cheese Bread\n", unidadeCheeseBread);
+        else if(unidadeCheeseBread>1)
+        printf("--%d Cheese Breads\n", unidadeCheeseBread);
+
+     if(unidadePizza==1)
+        printf("--%d Slice of Pizza\n", unidadePizza);
+        else if(unidadePizza>1)
+        printf("--%d Slices of Pizza\n", unidadePizza);
+
+     if(unidadeFries==1)
+        printf("--%d Portion of Fries\n", unidadeFries);
+        else if(unidadeFries>1)
+        printf("--%d Portions of Fries\n", unidadeFries);
+
+     if(unidadeHamburguer==1)
+        printf("--%d Hamburguer\n", unidadeHamburguer);
+        else if(unidadeHamburguer>1)
+        printf("--%d Hamburguers\n", unidadeHamburguer);
+
+     if(unidadeSoda==1)
+        printf("--%d Can of Soda\n", unidadeSoda);
+        else if(unidadeSoda>1)
+        printf("--%d Cans of Soda\n", unidadeSoda);
+
+     if(unidadeJuice==1)
+        printf("--%d Cup of Juice\n", unidadeJuice);
+        else if(unidadeJuice>1)
+        printf("--%d Cups of Juice\n", unidadeJuice);
+
+
+        printf("\nTotal: $%.1f", precoTotal);
+        printf("\n\n\nPayment Method:\n\n");
+        printf("1-Cash\n2-Credit Card\n3-Debit Card\n\n");
+        scanf("%d", &paymentChoice);
+        while(paymentChoice<1 || paymentChoice>3)
+        {
+            printf("\nInvalid Number, choose a number according to your payment method:\n\n");
+            printf("1-Cash\n2-Credit Card\n3-Debit Card\n\n");
+            scanf("%d", &paymentChoice);
+
+        }
+        int troco;
+        if(paymentChoice==1)
+        {  int loop=1;
+             while(loop==1)
+            {printf("\n1-Change\n2-No Change\n\n");
+             scanf("%d", &troco);
+             if(troco==1)
+             {
+             printf("\nTroco para quanto?\n");
+              scanf("%f", &vtroco);
+              while(vtroco<precoTotal)
+              {
+                  printf("\nNot enough money, type again\n");
+                  scanf("%f", &vtroco);
+              }
+              valordotroco=vtroco-precoTotal;
+              printf("\nTroco: $%.2f\n", valordotroco);
+              PedirEndereco();
+              return;
+             }
+             if(troco==2)
+                  {
+                   PedirEndereco();
+                   return;
+                  }
+             else
+                printf("\nINVALID COMMAND, TRY AGAIN\n");
+
+             }
+
+            }
+
+        PedirEndereco();
+
+        }
+
+void PedirEndereco()
+{
+    printf("\nType your adress:\n");
+    printf("\nCITY: ");
+    fflush(stdin);
+    scanf("%[^\n]", Adress[0]);
+    printf("DISTRICT: ");
+    fflush(stdin);
+    scanf("%[^\n]", Adress[1]);
+    printf("STREET: ");
+    fflush(stdin);
+    scanf("%[^\n]", Adress[2]);
+    printf("NUMBER: ");
+    fflush(stdin);
+    scanf("%[^\n]", Adress[3]);
+
+    system("cls");
+
+    printf("\nORDER PLACED!\n\n");
+    printf("**Order Summary**\n");
+    if(unidadeCoffee==1)
+        printf("--%d cup of Coffee\n", unidadeCoffee);
+        else if(unidadeCoffee>1)
+        printf("--%d cups of Coffee\n", unidadeCoffee);
+
+     if(unidadeCheeseBread==1)
+        printf("--%d Cheese Bread\n", unidadeCheeseBread);
+        else if(unidadeCheeseBread>1)
+        printf("--%d Cheese Breads\n", unidadeCheeseBread);
+
+     if(unidadePizza==1)
+        printf("--%d slice of Pizza\n", unidadePizza);
+        else if(unidadePizza>1)
+        printf("--%d slices of Pizza\n", unidadePizza);
+
+     if(unidadeFries==1)
+        printf("--%d portion of Fries\n", unidadeFries);
+        else if(unidadeFries>1)
+        printf("--%d portions of Fries\n", unidadeFries);
+
+     if(unidadeHamburguer==1)
+        printf("--%d Hamburguer\n", unidadeHamburguer);
+        else if(unidadeHamburguer>1)
+        printf("--%d Hamburguers\n", unidadeHamburguer);
+
+     if(unidadeSoda==1)
+        printf("--%d can of Soda\n", unidadeSoda);
+        else if(unidadeSoda>1)
+        printf("--%d cans of Soda\n", unidadeSoda);
+
+     if(unidadeJuice==1)
+        printf("--%d cup of Juice\n", unidadeJuice);
+        else if(unidadeJuice>1)
+        printf("--%d cups of Juice\n", unidadeJuice);
+
+printf("\nPayment Method:\n");
+if(paymentChoice==1)
+    printf("--Cash");
+else if(paymentChoice==2)
+    printf("--Credit Card");
+else if(paymentChoice==3)
+    printf("--Debit Card");
+if(valordotroco>0)
+    printf("\nTroco: $%.2f", valordotroco);
+
+printf("\n\nAdress: %s, %s, %s, %s.\n\n", Adress[0], Adress[1], Adress[2], Adress[3]);
+
+}
+
+int main()
+
+{
+
+    menu();
+
+}
+
+
---
+++ PROJETO/main.c
@@ -0,0 +1,379 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <strings.h>
+
+
+char usuario[100][10], senha[100][20], usuario1[10], senha1[20],checarsejatemcadastro[100][20], loki[100][20],escolhaProdutos[100][20], EnderecoEntrega[4][30], numero[100];
+int attempts=0, totalUsuarios=0, unidadeCoxinha=0, unidadePaodeQueijo=0, unidadeRocambole=0, unidadeBatataFrita=0, unidadeHamburguer=0, unidadeRefrigerante=0, unidadeSuco=0, escolhapagamento;
+float precoTotal=0, valordotroco, vtroco;
+
+
+
+void menu()
+{
+    printf("\n\t\t\t******Seja Bem vindo(a)******\n\n\t\t\t-DIGITE 1 PARA CADASTRAR USUÁRIO\n\t\t\t-DIGITE 2 PARA FAZER LOGIN\n\t\t\t-DIGITE SAIR PARA ENCERRAR O PROGRAMA.\n\n");
+    fflush(stdin);
+    scanf("%[^\n]", numero);
+    int sexo=1;
+    while(sexo==1)
+    {if(strcmp(numero,"1")==0)
+        {cadastrar();
+        return;
+        }
+
+    else if(strcmp(numero,"2")==0)
+        {login();
+        return;
+        }
+
+    else if(stricmp(numero,"sair")==0)
+        {printf("\nPROGRAMA ENCERRADO\n");
+        break;
+        }
+
+    else
+        {printf("\nCOMANDO INVÁLIDO, DIGITE NOVAMENTE\n");
+         fflush(stdin);
+         scanf("%[^\n]", numero);
+        }
+
+    }
+
+}
+
+void cadastrar()
+{
+    printf("\nDigite um nome de usuário para criar seu Login (sem espaços):\n\n");
+    scanf(" %[^\n]", usuario[totalUsuarios]);
+
+    while (strchr(usuario[totalUsuarios], ' ') != NULL)
+    {
+        printf("\nNome do usuário não pode conter espaços. Digite outro usuário:\n\n");
+        scanf(" %[^\n]", usuario[totalUsuarios]);
+    }
+
+
+    for(int i=0; i<totalUsuarios; i++)
+              {
+              while(strcmp(usuario[totalUsuarios], usuario[i])==0)
+                {printf("\nUsuário já cadastrado, digite outro usuário ou digite 3 para voltar ao menu:\n\n");
+                scanf("%s", usuario[totalUsuarios]);
+                if(strcmp(usuario[totalUsuarios], "3")==0)
+                {   system("cls");
+                    menu();
+                    return;
+                }
+                }
+              }
+    printf("\nAgora digite uma senha:\n\n");
+    scanf("%s", senha[totalUsuarios]);
+    totalUsuarios++;
+
+    printf("\nMuito Bem! Usuário e senha cadastrados com sucesso!\n\n");
+    printf("Pressione um tecla para voltar ao menu:\n\n");
+    getch();
+    system("cls");
+    menu();
+
+}
+
+void login()
+{
+    printf("\nPara entrar digite o seu usuário:\n\n");
+    scanf("%s", usuario1);
+
+    int achou=0;
+    int indexUsuario;
+    while (achou==0) {
+        for (int i = 0; i < totalUsuarios; i++) {
+            if (strcmp(usuario1,usuario[i])==0) {
+                achou = 1;
+                indexUsuario = i;
+                break;
+            }
+        }
+
+        if (achou==0) {
+            printf("\nUsuario não cadastrado, digite um usuário válido, ou pressione 3 para voltar ao menu:\n\n");
+            scanf("%s", usuario1);
+            if(strcmp(usuario1, "3")==0) {
+                system("cls");
+                menu();
+                return;
+            }
+        }
+    }
+
+    printf("\nDigite a senha:\n\n");
+    scanf("%s", senha1);
+
+    while(strcmp(senha[indexUsuario], senha1)!=0)
+
+        {   attempts++;
+            if(attempts==1)
+            {
+            printf("\nACESSO NEGADO, TENTE NOVAMENTE, 3 tentativas restantes:\n\n");
+            scanf("%s", senha1);
+            }
+            if(attempts==2)
+            {
+            printf("\nACESSO NEGADO, TENTE NOVAMENTE, 2 tentativas restantes:\n\n");
+            scanf("%s", senha1);
+            }
+            if(attempts==3)
+            {
+            printf("\nACESSO NEGADO, TENTE NOVAMENTE, 1 tentativa restantes:\n\n");
+            scanf("%s", senha1);
+            }
+            if(attempts==4)
+                printf("\nACESSO BLOQUEADO\n\n");
+
+    }
+
+    printf("\nACESSO GARANTIDO!\n\n");
+    printf("Pressione qualquer tecla para acessar a LANCHONETE...\n\n");
+    getch();
+    menulanchonete();
+
+}
+
+
+void menulanchonete()
+{
+    system("cls");
+    printf("\n\t******Seja Bem-vindo à Lanchonete Virtual******\n\n");
+
+    printf(" 1.Coxinha---------R$5,00\n");
+    printf(" 2.Pão de Queijo---R$4,00\n");
+    printf(" 3.Rocambole-------R$5,50\n");
+    printf(" 4.Batata Frita----R$6,00\n");
+    printf(" 5.Hambúrguer------R$5,00\n");
+    printf(" 6.Refrigerante----R$3,50\n");
+    printf(" 7.Suco------------R$3,00\n\n");
+
+    printf("Digite o seu pedido, item por item (máx 20 itens), pressione 0 para encerrar o seu pedido e ir para a forma de Pagamento:\n\n");
+
+    for(int i=1; i<=100;i++)
+    {
+        fflush(stdin);
+        scanf("%[^\n]", escolhaProdutos);
+        if(strcmp(escolhaProdutos,"0")==0)
+         {carrinho();
+         return;
+         }
+         if(stricmp(escolhaProdutos,"coxinha")==0 || strcmp(escolhaProdutos,"1")==0)
+           {precoTotal+=5;
+           unidadeCoxinha++;
+           }
+         else if(stricmp(escolhaProdutos,"pão de queijo")==0 || strcmp(escolhaProdutos,"2")==0)
+         {precoTotal+=4;
+         unidadePaodeQueijo++;
+         }
+        else if(stricmp(escolhaProdutos,"rocambole")==0 || strcmp(escolhaProdutos,"3")==0)
+           {precoTotal+=5.5;
+            unidadeRocambole++;
+           }
+        else if(stricmp(escolhaProdutos,"batata frita")==0 || strcmp(escolhaProdutos,"4")==0)
+         {precoTotal+=6;
+         unidadeBatataFrita++;
+         }
+        else if(stricmp(escolhaProdutos,"hambúrguer")==0 || strcmp(escolhaProdutos,"5")==0)
+           {precoTotal+=5;
+            unidadeHamburguer++;
+           }
+        else if(stricmp(escolhaProdutos,"refrigerante")==0 || strcmp(escolhaProdutos,"6")==0)
+         {precoTotal+=3.5;
+          unidadeRefrigerante++;
+         }
+        else if(stricmp(escolhaProdutos,"suco")==0 || strcmp(escolhaProdutos,"7")==0)
+           {precoTotal+=3;
+           unidadeSuco++;
+           }
+        else
+        {
+        printf("\nItem não cadastrado, digite novamente:\n\n");
+        }
+
+      }
+
+    }
+
+void carrinho()
+{
+     if(unidadeCoxinha==0 && unidadePaodeQueijo==0 && unidadeRocambole==0 && unidadeBatataFrita==0 && unidadeHamburguer==0 && unidadeRefrigerante==0 && unidadeSuco==0)
+        {
+         system("cls");
+         printf("\nEscolha pelo menos 1 item para continuar para o carrinho:\n\n");
+         printf("Pressione qualquer tecla para voltar ao cardápio...");
+         getch();
+         menulanchonete();
+
+
+        }
+    system("cls");
+    printf("\n**CARRINHO**\n");
+
+    if(unidadeCoxinha==1)
+        printf("--%d Coxinha\n", unidadeCoxinha);
+        else if(unidadeCoxinha>1)
+        printf("--%d Coxinhas\n", unidadeCoxinha);
+
+     if(unidadePaodeQueijo==1)
+        printf("--%d Pão de Queijo\n", unidadePaodeQueijo);
+        else if(unidadePaodeQueijo>1)
+        printf("--%d Pães de Queijo\n", unidadePaodeQueijo);
+
+     if(unidadeRocambole==1)
+        printf("--%d Rocambole\n", unidadeRocambole);
+        else if(unidadeRocambole>1)
+        printf("--%d Rocamboles\n", unidadeRocambole);
+
+     if(unidadeBatataFrita==1)
+        printf("--%d Batata Frita\n", unidadeBatataFrita);
+        else if(unidadeBatataFrita>1)
+        printf("--%d Batatas Fritas\n", unidadeBatataFrita);
+
+     if(unidadeHamburguer==1)
+        printf("--%d Hambúrguer\n", unidadeHamburguer);
+        else if(unidadeHamburguer>1)
+        printf("--%d Hambúrgueres\n", unidadeHamburguer);
+
+     if(unidadeRefrigerante==1)
+        printf("--%d Refrigerante\n", unidadeRefrigerante);
+        else if(unidadeRefrigerante>1)
+        printf("--%d Refrigerantes\n", unidadeRefrigerante);
+
+     if(unidadeSuco==1)
+        printf("--%d Suco\n", unidadeSuco);
+        else if(unidadeSuco>1)
+        printf("--%d Sucos\n", unidadeSuco);
+
+
+        printf("\nTotal a pagar: R$%.1f", precoTotal);
+        printf("\n\n\nQual a forma de pagamento:\n\n");
+        printf("1-Dinheiro\n2-Cartão de Crédito\n3-Cartão de Débito\n\n");
+        scanf("%d", &escolhapagamento);
+        while(escolhapagamento<1 || escolhapagamento>3)
+        {
+            printf("\nComando Inválido, escolha um número equivalente a sua forma de pagamento:\n\n");
+            printf("1-Dinheiro\n2-Cartão de Crédito\n3-Cartão de Débito\n\n");
+            scanf("%d", &escolhapagamento);
+
+        }
+        int troco;
+        if(escolhapagamento==1)
+        {  int loop=1;
+             while(loop==1)
+            {printf("\n1- Troco\n2- Sem Troco\n\n");
+             scanf("%d", &troco);
+             if(troco==1)
+             {
+             printf("\nTroco para quanto?\n");
+              scanf("%f", &vtroco);
+              while(vtroco<precoTotal)
+              {
+                  printf("\nDinheiro Insuficiente, digite outro valor:\n");
+                  scanf("%f", &vtroco);
+              }
+              valordotroco=vtroco-precoTotal;
+              printf("\nTroco: %.2f\n", valordotroco);
+              PedirEndereco();
+              return;
+             }
+             if(troco==2)
+                  {
+                   PedirEndereco();
+                   return;
+                  }
+             else
+                printf("\nCOMANDO INVÁLIDO, DIGITE NOVAMENTE\n");
+
+             }
+
+          }
+
+        PedirEndereco();
+
+
+        }
+
+void PedirEndereco()
+{
+    printf("\nDigite o seu endereço para entrega:\n");
+    printf("\nCIDADE: ");
+    fflush(stdin);
+    scanf("%[^\n]", EnderecoEntrega[0]);
+    printf("BAIRRO: ");
+    fflush(stdin);
+    scanf("%[^\n]", EnderecoEntrega[1]);
+    printf("NOME DA RUA: ");
+    fflush(stdin);
+    scanf("%[^\n]", EnderecoEntrega[2]);
+    printf("NÚMERO DA CASA: ");
+    fflush(stdin);
+    scanf("%[^\n]", EnderecoEntrega[3]);
+
+    system("cls");
+
+    printf("\nPEDIDO REALIZADO!\n\n");
+    printf("**Resumo do pedido**\n");
+    if(unidadeCoxinha==1)
+        printf("--%d Coxinha\n", unidadeCoxinha);
+        else if(unidadeCoxinha>1)
+        printf("--%d Coxinhas\n", unidadeCoxinha);
+
+     if(unidadePaodeQueijo==1)
+        printf("--%d Pão de Queijo\n", unidadePaodeQueijo);
+        else if(unidadePaodeQueijo>1)
+        printf("--%d Pães de Queijo\n", unidadePaodeQueijo);
+
+     if(unidadeRocambole==1)
+        printf("--%d Rocambole\n", unidadeRocambole);
+        else if(unidadeRocambole>1)
+        printf("--%d Rocamboles\n", unidadeRocambole);
+
+     if(unidadeBatataFrita==1)
+        printf("--%d Batata Frita\n", unidadeBatataFrita);
+        else if(unidadeBatataFrita>1)
+        printf("--%d Batatas Fritas\n", unidadeBatataFrita);
+
+     if(unidadeHamburguer==1)
+        printf("--%d Hambúrguer\n", unidadeHamburguer);
+        else if(unidadeHamburguer>1)
+        printf("--%d Hambúrgueres\n", unidadeHamburguer);
+
+     if(unidadeRefrigerante==1)
+        printf("--%d Refrigerante\n", unidadeRefrigerante);
+        else if(unidadeRefrigerante>1)
+        printf("--%d Refrigerantes\n", unidadeRefrigerante);
+
+     if(unidadeSuco==1)
+        printf("--%d Suco\n", unidadeSuco);
+        else if(unidadeSuco>1)
+        printf("--%d Sucos\n", unidadeSuco);
+
+printf("\nForma de pagamento:\n");
+if(escolhapagamento==1)
+    printf("--Dinheiro");
+else if(escolhapagamento==2)
+    printf("--Cartão de Crédito");
+else if(escolhapagamento==3)
+    printf("--Cartão de Débito");
+if(valordotroco>0)
+    printf("\nTroco: %.2f", valordotroco);
+
+printf("\n\nEndereço: %s, %s, %s, %s.\n\n", EnderecoEntrega[0], EnderecoEntrega[1], EnderecoEntrega[2], EnderecoEntrega[3]);
+
+}
+
+int main()
+
+{
+    setlocale(LC_ALL,"");
+
+    menu();
+
+
+}
+
